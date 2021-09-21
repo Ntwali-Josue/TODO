af57ff248c78dd6dd02119fbf9f575b67a066e77
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-21 23:54:53 +0200

    Add check props

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class TodoItem extends React.Component {
   render() {
-    return <li>{this.props.todo.title}</li>
+    return <li> <input type="checkbox" checked={this.props.todo.completed} onChange={()=> console.log('ndarenze')}/> {this.props.todo.title}</li>
   }
 }
 
