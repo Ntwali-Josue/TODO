f7d13e324b799f9dbb2dc5fd13857f59cead6d2e
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-22 11:58:24 +0200

    Validate the input field

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -13,11 +13,15 @@ class InputTodo extends React.Component {
   }
 
   handleSubmit = e => {
-    e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: ""
-    });
+    e.preventDefault()
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title)
+      this.setState({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    }
   }
 
   render() {
