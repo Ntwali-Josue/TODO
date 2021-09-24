fb62f2e0adef479a920ea170b8c2c6fd3dbe34f2
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-24 20:55:01 +0200

    Use componentDidMout to retrieve default todos

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -62,6 +62,12 @@ class TodoContainer extends React.Component {
     });
   };
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(response => response.json())
+      .then(data => this.setState({ todos: data }));
+  }
+
   render() {
     return (
       <>
